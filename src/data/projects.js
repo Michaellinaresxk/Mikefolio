dc4c615beba5f85c4fc847a8350bb90084319545
Michael Linares <michaellinaresdbildungs@gmail.com>
2024-02-15 01:19:16 +0100

    Added data and immages to the project.

---
+++ src/data/projects.js
@@ -0,0 +1,41 @@
+
+
+[
+  {
+    id: 1,
+    title: "Words Translator",
+    subtitle: "Learn other languages...",
+    description: "It’s a Small project to providing translation for the most important languages around the world, I made this personal app because I wanted to learn polish, Italian and Russian languages, and to do it with one tool created by me.",
+    CardImage: "../assets/img/projects/project1.jpg",
+    projectLinks: "https://word-translator-70f04.web.app/",
+    repoLink: "https://github.com/Michaelxk/Vue-Translator",
+    technologies: ["Git", "Materialize", "Vue.JS", "Vue-Resourse", "REST API: Yandex", "Firebase",],
+    whatIImprube: "I learn how to integrate Vue with Vue-Resourse for to make the request to Yandex API, chanching Bootstrap CSS Framework for Materialize, trying to dont repeat myself.",
+    appImage: "../assets/img/projects/project-details/translator.png"
+  },
+  {
+    id: 2,
+    title: "Gosia Recipe",
+    subtitle: "Be Smart, Eat Smart...",
+    description: "Search for recipes using natural language queries, such as 'gluten free brownies without sugar' or 'low fat vegan cupcakes.' You can automatically calculate the nutritional information for any recipe, visualize ingredient lists, etc..",
+    CardImage: "../assets/img/projects/project2.jpg",
+    projectLinks: "https://wizardly-spence-eb6793.netlify.app/gosia-recipe",
+    repoLink: "https://github.com/Michaelxk/Gosia-Recipe",
+    technologies: ["Git", "Bootstrap-Vue", "Vue.JS", "Axios", "REST API: Food Nutrition (Rapid-Api)", "Firebase"],
+    whatIImprube: "This is the project that i like the most, because i enjoy a lot creating routes, bringing the recipes from Food Nutrition API, and providing to the people good and nice UI and UX experience.",
+    appImage: "../assets/img/projects/project-details/gosia.png"
+  },
+  {
+    id: 3,
+    title: "Jammusik",
+    subtitle: "",
+    description: "",
+    CardImage: "",
+    projectLinks: "https://jammusik-dc856.web.app/",
+    repoLink: "https://gitlab.com/Michaelxk/jammusik",
+    technologies: ["Vue", "Tyescript", "Firebase", "Clean Architecture"],
+    whatIImprube: "",
+    appImage: ""
+  },
+
+]
