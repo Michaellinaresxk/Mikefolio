5ab9b6ad2355270006a36a61b3716eeee27e7c05
Michael Linares <michaellinaresdbildungs@gmail.com>
2024-04-08 10:26:42 +0200

    Added hero image to about page.

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,5 +1,6 @@
 import photo1 from "./1.jpg";
 import phot02 from "./sm.jpg";
 import background_image from "./background.jpg";
+import about_background_image from "./banner.jpg";
 
-export { photo1, phot02, background_image };
+export { photo1, phot02, background_image, about_background_image };
