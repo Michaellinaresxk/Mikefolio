7bee9e4c63d6b3856104543c66ee917fb91f3a24
Michael Linares <michaellinaresdbildungs@gmail.com>
2024-04-08 23:35:19 +0200

    added creative banner.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,20 @@ import Menu from "../components/stateless/Menu";
 import CreativeTitleBanner from "../components/stateless/CreativeTitleBanner";
 
 const Home = () => {
-	return (
-		<main>
-			<div className="bg-home transition-all ease-in">
-				<div className="w-screen h-screen bg-slate-900 bg-opacity-70  w-full justify-center flex flex-grow h-full items-center">
-					<Menu />
-					<Presentation
-						title1="Michael"
-						title2="Linares"
-						subtitle="FrontEnd Developer & Web Designer"
-					/>
-				</div>
-			</div>
-			<CreativeTitleBanner />
-		</main>
-		
-	);
+  return (
+    <main>
+      <div className="bg-home transition-all ease-in">
+        <div className="w-screen h-screen bg-slate-900 bg-opacity-0  w-full flex flex-grow h-full items-center pl-20">
+          <Menu />
+          <Presentation
+            title1="Michael"
+            title2="Linares"
+            subtitle="FrontEnd Developer & Web Designer"
+          />
+        </div>
+      </div>
+      <CreativeTitleBanner />
+    </main>
+  );
 };
 export default Home;
